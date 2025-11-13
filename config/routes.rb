113d0c70fa66a9396770ca7a6b17aea1516192f2
Felipe Silva <talk.felipe@gmail.com>
2025-11-13 16:59:13 +0900

    finished the challenge without the optionals

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks
+  end
+
+  resources :bookmarks, only: [:destroy]
 end
